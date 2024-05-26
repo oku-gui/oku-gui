818b227ae2153e36fad56fda13d9aab57b3d7b37
Austin M. Reppert <austinmreppert@protonmail.com>
2024-05-25 21:54:40 -0400

    We got on click working :0

--- examples/test_binary/test_binary.rs
+++ examples/test_binary/test_binary.rs
@@ -5,6 +5,7 @@ use oku::elements::element::Element;
 use oku::elements::text::Text;
 use oku_core::events::EventResult;
 use oku_core::reactive::reactive;
+use oku_core::reactive::reactive::RUNTIME;
 use oku_core::OkuOptions;
 use oku_core::RendererType::Wgpu;
 use std::sync::Arc;
@@ -21,18 +22,12 @@ struct Hello {}
 
 impl Component<u64> for Hello {
     fn view(&self, props: Option<&Props>, key: Option<String>) -> Element {
-        let mut container = Container::new().add_child(Element::Text(Text::new(format!("Hello, world! {}", 5))));
+        if RUNTIME.get_state::<u32>(0).is_none() {
+            RUNTIME.set_state(0, 0u32);
+        }
 
-        let key = 1;
-        let value = 5u64;
-
-        self.set_state(5);
-        let x = self.get_state().unwrap();
-        //println!("x: {}", x);
-
-        /*for child in children {
-            container = container.add_child(child);
-        }*/
+        let x: u32 = RUNTIME.get_state(0).unwrap();
+        let container = Container::new().add_child(Element::Text(Text::new(format!("Hello, world! {}", x))));
 
         let mut custom_component = oku::elements::component::Component::new();
         custom_component = custom_component.add_child(Element::Container(container));
@@ -40,6 +35,9 @@ impl Component<u64> for Hello {
         custom_component.add_update_handler(Arc::new(|msg, state| {
             println!("msg: {:?}", 2);
             println!("state: {:?}", 2);
+
+            let x: u32 = RUNTIME.get_state(0).unwrap();
+            RUNTIME.set_state(0, x + 1);
         }));
 
         Element::Component(custom_component)
