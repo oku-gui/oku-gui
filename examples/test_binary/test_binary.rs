721bcb2afa9b108072d3b37308c728f49623cc00
NoahR02 <noahreppert95@gmail.com>
2024-07-05 19:03:48 -0400

    Use user imports for the test_binary example

--- examples/test_binary/test_binary.rs
+++ examples/test_binary/test_binary.rs
@@ -1,28 +1,26 @@
-use oku::components::component::Component;
+use oku::elements::style::{FlexDirection, Unit};
+use oku::components::component::UpdateFn;
+use oku::components::component::ComponentOrElement;
+use oku::components::component::{Component, ComponentSpecification};
 use oku::elements::container::Container;
 use oku::elements::text::Text;
-use oku_core::components::component::{ComponentOrElement, ComponentSpecification, UpdateFn};
-use oku_core::components::props::Props;
-use oku_core::elements::element::Element;
-use oku_core::elements::empty::Empty;
-use oku_core::elements::style::{AlignItems, FlexDirection, JustifyContent, Unit};
-use oku_core::events::{EventResult, Message};
-use oku_core::reactive::reactive;
-use oku_core::reactive::reactive::RUNTIME;
-use oku_core::renderer::color::Color;
-use oku_core::RendererType::Wgpu;
-use oku_core::{component, OkuOptions};
 use std::any::Any;
+use oku::{component, oku_main_with_options, OkuOptions};
+use oku::components::props::Props;
+use oku::events::Message;
+use oku::reactive::reactive::RUNTIME;
+use oku::renderer::color::Color;
+use oku::RendererType::Wgpu;
 use rand::seq::SliceRandom;
 
 pub fn counter(_props: Option<Props>, children: Vec<ComponentSpecification>, id: u64) -> (ComponentSpecification, Option<UpdateFn>) {
     let count = RUNTIME.get_state(id).unwrap_or(0u32);
 
     println!("Children len: {}", children.len());
-    
+
     //let mut component_children = children.clone();
     //component_children.push(Text::new(format!("Counter Count: {}", count).as_str()).into());
-    
+
     (
         ComponentSpecification {
             component: Text::new(format!("Counter Count: {}", count).as_str()).into(),
@@ -64,7 +62,7 @@ pub fn app(_props: Option<Props>, children: Vec<ComponentSpecification>, id: u64
         },
     ];
     children2.shuffle(&mut rng);
-    
+
     (
         ComponentSpecification {
             component: Container::new().background(Color::new_from_rgba_u8(240, 255, 220, 255)).width(Unit::Px(900.0)).height(Unit::Px(900.0)).flex_direction(FlexDirection::Column).into(),
@@ -109,7 +107,7 @@ pub fn update(id: u64, message: Message) {
 }
 
 fn main() {
-    oku_core::oku_main_with_options(
+    oku_main_with_options(
         ComponentSpecification {
             component: component!(app),
             key: None,
